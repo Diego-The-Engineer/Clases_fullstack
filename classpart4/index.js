58ccd8b755d9f8d576ef218216a81e895ed40355
Diego <diegoaimargdlr@gmail.com>
2026-09-21 08:41:42 -0600

    Cambiando configs

--- classpart4/index.js
+++ classpart4/index.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+import express from 'express'
+import cors from 'cors'
 
+const app = express()
+app.use(express.json())
 app.use(cors())
+
 let notes = [
   {
     id: "1",
